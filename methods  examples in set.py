8b3d667bf3f30c1664501af926355243217fcb64
Dinuhub-tech <127160011@sastra.ac.in>
2026-06-21 12:38:47 +0530

    Create methods  examples in set.py

---
+++ methods  examples in set.py
@@ -0,0 +1,7 @@
+dhoni={"dinesh","balaji","jathin","krishna","sumanth"}
+kohli={"pardhu","akshaya","krishna","sumanth"}
+print(dhoni.union(kohli))
+print(dhoni.intersection(kohli))
+print(dhoni.difference(kohli))
+print(kohli.difference(dhoni))
+print(dhoni.symmetric_difference(kohli))
