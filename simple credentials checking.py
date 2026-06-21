b2ed15f54f11cfae25fa5e6614cdefb333f43bcb
Dinuhub-tech <127160011@sastra.ac.in>
2026-06-21 12:37:20 +0530

    Create simple credentials checking.py

---
+++ simple credentials checking.py
@@ -0,0 +1,9 @@
+
+user_name=input()
+password=input()
+login_user_name=input()
+login_pass=input()
+if user_name == login_user_name and password ==login_pass:
+  print("Login successfully")
+else:
+  print("Credentials are wrong")
